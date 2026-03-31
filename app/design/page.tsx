ddcb54363c5b9b030723032684e036a4176ff4da
Emily Clarkson <erc.clarkson@btinternet.com>
2026-03-31 18:19:44 +0100

    made sure height is screen on page for scroll

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -2,7 +2,7 @@ import LeagueTable from "../components/league-table";
 
 export default function Page() {
   return (
-    <main className="w-full max-w-2xl place-self-center shadow-md rounded-md border border-gray-200">
+    <main className="shadow-md rounded-md border border-gray-200 h-screen overflow-hidden">
       <LeagueTable />
     </main>
   );
