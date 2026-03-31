00898fea610475c7c47a1dc446d9dcd5769d8048
Emily Clarkson <erc.clarkson@btinternet.com>
2026-03-31 18:25:19 +0100

    home page link

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import LeagueTable from "../components/league-table";
 
 export default function Page() {
   return (
-    <main className="shadow-md rounded-md border border-gray-200 h-screen overflow-hidden">
-      <LeagueTable />
+    <main className="grid grid-cols-1 gap-8">
+      <div className="shadow-md rounded-md border border-gray-200 h-screen overflow-hidden">
+        <LeagueTable />
+      </div>
+      <Link
+        href="/"
+        className="underline text-blue-500 hover:text-blue-600 underline-offset-4 w-max text-xs font-semibold uppercase"
+      >
+        Home page
+      </Link>
     </main>
   );
 }
